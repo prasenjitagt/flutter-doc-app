eb2c165bf82ff9e896ad4a50440c358ac16d2d11
prasenjitagt <prasanjitacademic@gmail.com>
2023-11-16 01:25:45 +0530

    Removed unnecessary cupertino import from login screen

--- lib/views/login_screen.dart
+++ lib/views/login_screen.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:medi/views/forgot_pass.dart';
 import 'package:medi/views/home_screen.dart';
